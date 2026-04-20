65846514d07cd32edf5ec30c2a27878532062ef5
Fernando Arteaga <fernandoarteagacortez@gmail.com>
2026-04-20 11:20:33 -0600

    Agregar el resto del código

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeguridadApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+
+    [HttpPost]
+    public IActionResult Login(string username)
+    {
+        if (string.IsNullOrEmpty(username)) return RedirectToAction("Index");
+        
+        // Guardamos el nombre en la sesión
+        HttpContext.Session.SetString("UsuarioLogueado", username);
+        return RedirectToAction("Dashboard");
+    }
+
+    public IActionResult Dashboard() => View();
+    public IActionResult Bicicletas() => View();
+    public IActionResult Sensible() => View();
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+}
